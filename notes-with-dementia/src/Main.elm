5a10e443faa47cacabffdde50a4c7b428891d52c
Michael Jonathan Lee <code.mjlee@gmail.com>
2018-09-29 23:41:40 +0200

    Added deletion of notes.

--- notes-with-dementia/src/Main.elm
+++ notes-with-dementia/src/Main.elm
@@ -4,56 +4,80 @@ import Html exposing (div, a, form, text, nav, span, input, label, i)
 import Html.Attributes exposing (class, type_, value)
 import Html.Events exposing (onInput, onClick)
 import Browser
+import Random exposing (Seed, initialSeed, step)
+import Uuid exposing (Uuid, uuidGenerator)
 
 type alias State = {
         notes: List Note,
         title: String,
-        body: String
+        body: String,
+        currentSeed: Seed
     }
 
-initialState = {
+initialState seed =
+    {
         notes = [],
         title = "",
-        body = ""
+        body = "",
+        currentSeed = initialSeed seed
     }
 
-type Action = ADD | CHANGE_TITLE String | CHANGE_BODY String | DELETE Int | NONE
+type Action = ADD | CHANGE_TITLE String | CHANGE_BODY String | DELETE_NOTE Uuid | NONE
 
 reducer action state = case action of
     CHANGE_TITLE title -> {state | title = title}
     CHANGE_BODY body -> {state | body = body}
-    ADD -> {state |
-            notes = addNote state.notes state.title state.body,
-            title = "",
-            body = ""
-        }
-    DELETE index -> state
+    ADD ->
+        let
+           (uuid, newSeed) = step uuidGenerator state.currentSeed
+        in
+            {state |
+                notes = addNote state.notes uuid state.title state.body,
+                title = "",
+                body = "",
+                currentSeed = newSeed
+                
+            }
+    DELETE_NOTE uuid ->
+        let
+            notThisNote = isNotNote uuid
+        in
+            {state |
+               notes = List.filter notThisNote state.notes
+            }
     _ -> state
 
 
+isNotNote id note = not (id == note.id)
+
 type alias Note = {
+        id: Uuid,
         title: String,
         body: String
     }
 
-note title body = {
+createNote id title body = {
+        id = id,
         title = title,
         body = body
     }
 
-addNote notes title body =
-    List.append notes [note title body]
+addNote notes id title body =
+    List.append notes [createNote id title body]
 
-renderNote {title, body} =
-    div [class "card"][
-        div [class "card-content"][
-            span [class "card-title"][text title],
-            text body,
-            div [class "card-action"][
-                a [][text "remove"]
+renderNote {id, title, body} =
+    let
+        handleDelete = DELETE_NOTE id
+    in
+        div [class "card"][
+            div [class "card-content"][
+                span [class "card-title"][text title],
+                text body,
+                div [class "card-action"][
+                    a [onClick handleDelete][text "remove"]
+                ]
             ]
         ]
-    ]
 
 render state =
     div[class "row"][
@@ -77,8 +101,22 @@ render state =
     ]
 
 
-main = Browser.sandbox {
-        init = initialState,
-        update = reducer,
-        view = render
+init seed =
+    (
+        initialState seed,
+        Cmd.none
+    )
+
+update action state = (
+        reducer action state,
+        Cmd.none
+    )
+
+subscriptions _ = Sub.none
+
+main = Browser.element {
+        init = init,
+        update = update,
+        view = render,
+        subscriptions = subscriptions
     }
